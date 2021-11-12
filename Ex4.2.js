11ea68a528e6d43f882934fd3882a6eeb0330f23
rajaaghanem <rajaaghanem@gmail.com>
2021-11-12 10:37:54 +0200

    4.2

--- Ex4.2.js
+++ Ex4.2.js
@@ -1,2 +1,19 @@
 //$ Ex4.2 - Tribonacci 
 
+function tribonacci(signature, n){
+    let arr =[];
+    arr[0]=signature[0];
+    arr[1]=signature[1];
+    arr[2]=signature[2];
+
+    for(let i=3; i<=n; i++){
+        arr[i]=arr[i-1]+arr[i-2]+arr[i-3];
+    }
+    return arr;
+}
+
+
+
+//! test
+let arr= [1,1,1];
+console.log(tribonacci(arr,8));
