23c8cae5000061d666d4691203692080bf12b151
rajaaghanem <rajaaghanem@gmail.com>
2021-11-12 10:22:02 +0200

    4.1

---
+++ Ex4.1.js
@@ -0,0 +1,13 @@
+//$ Ex4.1 - Fibonacci -
+
+function fibonacci(n){
+    let arr = [];
+    arr[0]=0;
+    arr[1]=1;
+    for (let i=2; i<=n ; i++){
+        arr[i]=arr[i-1]+ arr[i-2];
+    }
+    return arr[n];
+}
+
+console.log(fibonacci(7));
